f1a8654ea7a4d46ee00a2bee904b3145773e36a9
Nandun Nethmina <e19170@eng.pdn.ac.lk>
2023-09-06 23:14:28 +0530

    completed

---
+++ frontend/frontend/src/pages/Home.js
@@ -0,0 +1,41 @@
+import {useEffect, useState} from 'react'
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
+
+import WorkoutDetails from '../components/WorkoutDetails'
+import WorkoutForm from '../components/WorkoutForm'
+
+const Home = () => {
+  const {workouts,dispatch}=useWorkoutsContext()
+
+  //const [workouts,setWorkouts] = useState(null)
+
+  useEffect(()=>{
+    const fetchWorkout = async ()=>{
+      const response = await fetch('/api/workouts')
+      const json = await response.json()
+
+      if (response.ok){
+        //setWorkouts(json)
+        dispatch({type:'SET_WORKOUTS',payload:json})
+      }
+    }
+
+    fetchWorkout()
+
+  },[dispatch])
+
+
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts && workouts.map((workout)=>(
+          <WorkoutDetails key={workout._id} workout={workout}/>
+          
+        ))}
+      </div>
+      <WorkoutForm />
+    </div>
+  )
+}
+
+export default Home
